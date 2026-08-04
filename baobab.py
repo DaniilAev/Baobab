48f680ab1c073aeafb6f32f10ac8eb959a3ad344
DaniilAev <daniilaev415@gmail.com>
2026-08-04 16:43:04 +0300

    индексация файлов в share

--- baobab.py
+++ baobab.py
@@ -1,2 +1,12 @@
-import socket, ssl
+import socket, ssl, os, json, threading
+def main():
+    dir_path = "share\\"
+    os.makedirs(dir_path, exist_ok=True)
+    share_list = os.listdir(dir_path)
+    files_json = json.dumps({file_name: os.path.getsize(dir_path + file_name) for file_name in share_list}, ensure_ascii=False)
+    file_descriptors = {file_name: open(dir_path + file_name, 'rb') for file_name in share_list}
 
+
+
+
+main()
